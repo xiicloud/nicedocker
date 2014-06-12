6eaadf163967f5c281d7a5a2b8979f0c7ed9ce42
William Hanwoody <hanwoody@gmail.com>
2014-06-12 17:15:24 +0800

    logrotate/cron support change

--- docker_init.sh
+++ docker_init.sh
@@ -9,8 +9,6 @@ WDIR=/tmp/docker_$tv
 NICESCALEDIR=/opt/nicescale/support
 [ -d $NICESCALEDIR/bin ] || mkdir -p $NICESCALEDIR/bin
 [ -d $NICESCALEDIR/etc ] || mkdir -p $NICESCALEDIR/etc
-logrotate_file=$NICESCALEDIR/etc/logrotate-services.conf
-cron_logrotate_script=/etc/cron.daily/logrotate-services
 #[ -d $NICESCALEDIR/etc ] || mkdir -p $NICESCALEDIR/etc
 
 SERVICE_TYPES="mysql redis memcached apache_php haproxy tomcat percona-mysql"
@@ -79,28 +77,6 @@ ln -sf $NICESCALEDIR/bin/nicedocker /usr/local/bin/nicedocker
 ln -sf $NICESCALEDIR/bin/nicedocker /usr/local/bin/dockernice
 ln -sf $NICESCALEDIR/bin/nsexec /usr/local/bin/nsexec
 
-cat <<EOF > $cron_logrotate_script
-#!/bin/sh
-
-test -x /usr/sbin/logrotate || exit 0
-/usr/sbin/logrotate $logrotate_file
-EOF
-chmod a+x $cron_logrotate_script
-
-cat <<EOF > $logrotate_file
-# see "man logrotate" for details
-# rotate log files weekly
-weekly
-
-# keep 4 weeks worth of backlogs
-rotate 4
-
-# create new (empty) log files after rotating old ones
-create
-
-# include service's logrotate.d directory
-
-EOF
 repohost=`get_repo`
 for s in $SERVICE_TYPES; do
   docker pull $repohost:5000/nicescale/$s
